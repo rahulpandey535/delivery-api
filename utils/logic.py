db7a7467f2abb2988f12b72bb8e1af96a73bb364
Rahul Pandey <rahulpandey02124@gmail.com>
2025-04-19 11:02:48 +0530

    Initial commit

--- utils/logic.py
+++ utils/logic.py
@@ -6,32 +6,53 @@ product_centers = {
     'G': ['C3'], 'H': ['C3'], 'I': ['C3']
 }
 
-distances = {
+# Distances between centers and L1
+distances_to_l1 = {
     'C1': 10,
     'C2': 30,
     'C3': 33
 }
 
+# Distances between centers (assumed symmetric for simplicity)
+center_distances = {
+    ('C1', 'C2'): 20,
+    ('C1', 'C3'): 25,
+    ('C2', 'C3'): 15
+}
+# Add reverse mappings
+for (a, b), d in list(center_distances.items()):
+    center_distances[(b, a)] = d
+
 COST_PER_KM = 2
 
 def calculate_minimum_delivery_cost(order):
+    # Step 1: Find which centers are needed based on the order
     centers_needed = set()
-    
+
     for product, qty in order.items():
         if qty > 0 and product in product_centers:
-            # Choose the center with the shortest distance if multiple
-            best_center = min(product_centers[product], key=lambda c: distances[c])
+            best_center = min(product_centers[product], key=lambda c: distances_to_l1[c])
             centers_needed.add(best_center)
 
     if not centers_needed:
         return 0
 
-    min_cost = float('inf')
+    min_cost = float("inf")
 
+    # Step 2: Try all permutations of centers to find optimal delivery path
     for perm in permutations(centers_needed):
         cost = 0
-        for center in perm:
-            cost += 2 * distances[center] * COST_PER_KM  # round trip
+        route = list(perm) + ['L1']
+
+        for i in range(len(route) - 1):
+            from_loc = route[i]
+            to_loc = route[i + 1]
+
+            if to_loc == 'L1':
+                cost += distances_to_l1[from_loc] * COST_PER_KM
+            else:
+                cost += center_distances.get((from_loc, to_loc), float("inf")) * COST_PER_KM
+
         min_cost = min(min_cost, cost)
 
     return min_cost
